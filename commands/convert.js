6fc0f1f9f78fbad0928486760b06cfb63d08603e
Nisha Singhal <nishasinghal979@gmail.com>
2023-10-26 19:51:23 +0530

    Improve output display of convert command

--- commands/convert.js
+++ commands/convert.js
@@ -1,5 +1,6 @@
 import { Command } from "commander";
 import sharp from "sharp";
+import { extname } from "path";
 
 const convertCommand = new Command();
 
@@ -9,10 +10,19 @@ convertCommand
   .description('Convert an image to another format')
   .action(async (inputFile, outputFile) => {
     try {
+      const inputFormat = extname(inputFile);
+      const outputFormat = extname(outputFile);
+
+      // throw error if input and outfile have same extension
+      if (inputFormat == outputFormat){
+        throw Error(`The format of <inputFile> and <outputFile> cannot be same. Both files have same format: ${inputFormat}`);
+      }
+
+      // convert file format
       const info = await sharp(inputFile).toFile(outputFile);
-      console.log("Image converted:", info);
+      console.log(`Image converted from ${inputFormat} to ${outputFormat} format.`, );
     } catch (err) {
-      console.error(err);
+      console.error(err.message);
     }
   });
 
