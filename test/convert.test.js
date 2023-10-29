915458b2fd447723c9cd21f46f12955e6c4645c3
Nisha Singhal <nishasinghal979@gmail.com>
2023-10-29 22:35:34 +0530

    Add unit tests for convert command

--- test/convert.test.js
+++ test/convert.test.js
@@ -1,20 +1,56 @@
-const { execSync } = require('child_process');
-const { expect } = require('chai');
+// Import necessary modules
+import { execSync } from 'child_process';
+import { expect } from 'chai';
+import fs from 'fs';
 
-describe('Convert Command', function () {
+
+// Describe block for 'convert' command tests
+describe('Convert Command Tests', function () {
+  
+  // Test case 1: Proper command
   it('should convert image from PNG to JPEG', function () {
-    const result = execSync('npx img-cli convert ./myimg.png ./myimg.jpeg').toString();
-    expect(result).to.include('Conversion successful');
+    const result = execSync('node ./index.js convert ./myimg.png ./myimg.jpeg').toString();
+    expect(result).to.include('Image converted from .png to .jpeg');
   });
 
-  it('should handle missing input file for convert command', function () {
-    const result = execSync('npx img-cli convert').toString();
-    expect(result).to.include('Error: Missing input file');
+  // Test case 2: When outputFile arg not given
+  it('should handle missing output file argument', function () {
+    try {
+      // Run the command and capture the result
+      const result = execSync('node ./index.js convert ./myimg.png');
+    } catch (error) {
+      // Check if the expected error message is present in the stderr
+      const errMsg = "error: missing required argument 'outputFile'";
+      expect(error.stderr.toString()).to.include(errMsg);
+    }
+  });
+  // // Test case 3: When both the args outputFile and inputFile are missing
+  it('should handle missing input and output file arguments', function () {
+    try {
+      // Run the command and capture the result
+      const result = execSync('node ./index.js convert');
+    } catch (error) {
+      // Check if the expected error message is present in the stderr
+      const errMsg = "error: missing required argument 'inputFile'";
+      expect(error.stderr.toString()).to.include(errMsg);
+    }
   });
 
-  // Add more test cases for the convert command
+  // // Test case 4: When inputFile doesn't exist
+  it('should handle non-existent inputFile argument', function () {
+    try {
+      // Run the command and capture the result
+      const result = execSync('node ./index.js convert ./myimg.jpg ./myimg.tiff');
+    } catch (error) {
+      // Check if the expected error message is present in the stderr
+      const errMsg = "Input file is missing: ./myimg.jpg";
+      expect(error.stderr.toString()).to.include(errMsg);
+    }
+  });
 
+  // Cleanup after testing
   after(function () {
-    // Add cleanup or teardown logic if needed
+    // Delete temporary file created during testing
+    fs.unlinkSync('./myimg.jpeg');
   });
 });
