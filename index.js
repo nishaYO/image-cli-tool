3ff8032a97f8008acd0e9cb2ceb3b416087468fb
Nisha Singhal <nishasinghal979@gmail.com>
2023-10-29 09:47:29 +0530

    Bumped version to 2.2.0

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ async function imageCliTool() {
     .description(
       "Image CLI tool that helps in manipulating images using your terminal."
     )
-    .version("2.1.0");
+    .version("2.2.0");
 
   // add imported module commands
   commander.addCommand(compressCommand);
