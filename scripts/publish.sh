956128ced2b9738dae70fc810183d29039d42607
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-04 13:05:02 +0530

    Improve publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Function to prompt user for confirmation
+confirm() {
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+
 # Get the version from user input
 read -p "Enter the version (e.g., 3.0.0): " version
 
@@ -7,14 +21,17 @@ read -p "Enter the version (e.g., 3.0.0): " version
 npm version $version
 sed -i "s/version('.*')/version('$version')/" index.js
 
-npm run test
+# Run tests and proceed only if tests pass
+if npm run test; then
+    # Commit and tag the changes
+    git add .
+    git commit -m "Bumped version to $version"
+    git tag -a "v$version" -m "Version $version"
+    git push origin main
+    git push --tags
 
-# Commit and tag the changes
-git add .
-git commit -m "Bumped version to $version"
-git tag -a "v$version" -m "Version $version"
-git push origin main
-git push --tags
-
-# Publish to npm
-npm publish
+    # Confirm before publishing to npm
+    confirm "Do you want to publish to npm? [y/N]" && npm publish
+else
+    echo "Tests failed. Aborting publish process."
+fi
