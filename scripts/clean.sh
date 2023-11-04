fc7b07defb5c662b99c754ba7a92290f46de2c50
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-04 10:08:46 +0530

    Create separate dir for shell scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Delete files starting with "myimg" except for "myimg.png"
-find . -name 'myimg*' ! -name 'myimg.png' -exec rm {} +
+find ../ -name 'myimg*' ! -name 'myimg.png' -exec rm {} +
 
 echo "Files deleted successfully."
