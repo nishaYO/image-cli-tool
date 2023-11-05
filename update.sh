7b0374e594496b65de3d465c91c804b5f3f47e6b
Nisha Singhal <nishasinghal979@gmail.com>
2023-11-05 16:42:59 +0530

    automate tests, commit and branch sync

---
+++ update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Function to prompt user for confirmation
+confirm() {
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+# Get commit name from user input
+read -p "Enter commit name: " commit_name
+
+# Run tests and proceed only if tests pass
+if npm test; then
+    # Tests passed, proceed with commit and push
+    git add .
+    git commit -m "$commit_name"
+
+    # Push to the current branch
+    git push
+
+    # Ask for confirmation to merge dev into main
+    if confirm "Do you want to merge dev into main? (y/n): "; then
+        git checkout main
+        git pull origin main
+        git merge dev
+        git push origin main
+        git checkout dev
+    else
+        echo "Skipping merge of dev into main."
+    fi
+
+    echo "Update script completed successfully."
+else
+    # Tests failed, display an error message
+    echo "Tests failed. Please fix the issues before committing."
+fi
